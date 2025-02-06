102543495943515ecc62cf56e2e662886c5b0b51
benedictwaweru <benedictwawerum@gmail.com>
2025-02-06 14:55:03 +0300

    first commit

---
+++ app/die_roller.py
@@ -0,0 +1,14 @@
+'''
+Roll a die 1000 times. Use a generator that generates random numbers between 0 and 1 and give results in a table with the following:
+
+Face			Frequency			Percentage(1dp)
+1
+2
+3
+4
+5
+6 				
+
+Face			Must add to   Must add to
+						1000           100.0
+'''
